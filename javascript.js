a5d7e8565d5dde0e59c4b74c3a42938bdbf798ab
Digvijay Singh <digvijaysingh3344@gmail.com>
2025-12-30 13:58:32 +0530

    Project complete

--- javascript.js
+++ javascript.js
@@ -4,4 +4,56 @@ function getComputerChoice(){
     else if(rand>2/3) return "paper"
     else return "scissors"
 }
-console.log(getComputerChoice());
+
+function getHumanChoice(){
+    return prompt("rock, paper or scissors?");
+}
+
+function playGame(){
+    let humanScore=0, computerScore=0;
+
+    let playRound=function(humanChoice,computerChoice){
+        humanChoice.toLowerCase();
+        if(humanChoice==computerChoice) console.log('Tie!');
+        else if(humanChoice=='rock'){
+            if(computerChoice=='paper'){
+                console.log('You lose! Paper beats Rock')
+                computerScore++;
+            }
+            else if(computerChoice=='scissors'){
+                console.log('You win! Rock beats Scissors')
+                humanScore++;
+            }
+        }
+        else if(humanChoice=='paper'){
+            if(computerChoice=='rock'){
+                console.log('You win! Paper beats Rock')
+                humanScore++;
+            }
+            else if(computerChoice=='scissors'){
+                console.log('You lose! Scissors beats Paper')
+                computerScore++;
+            }
+        }
+        else {
+            if(computerChoice=='rock'){
+                console.log('You lose! Rock beats Scissors')
+                computerScore++;
+            }
+            else if(computerChoice=='paper'){
+                console.log('You win! Scissors beats Paper')
+                humanScore++;
+            }
+        }
+    }
+
+    for(let i=0;i<5;i++){
+        playRound(getHumanChoice(),getComputerChoice());
+    }
+
+    if(humanScore==computerScore) console.log("Tie!");
+    else (humanScore>computerScore)?console.log("You Win!"):console.log("Computer Wins!");
+    
+}
+
+playGame();
