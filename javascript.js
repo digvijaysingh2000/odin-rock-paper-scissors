3772e7b3dd1319537069d4e30b664857ff372856
Digvijay Singh <digvijaysingh3344@gmail.com>
2025-12-30 13:08:05 +0530

    Random function created

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function getComputerChoice(){
+    let rand=Math.random();
+    if(rand<1/3) return "rock";
+    else if(rand>2/3) return "paper"
+    else return "scissors"
+}
+console.log(getComputerChoice());
